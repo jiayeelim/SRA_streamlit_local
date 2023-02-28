8a8ecdead6ee795b228b9d10f69db7fdb642e5e4
jiayeelim <73089478+jiayeelim@users.noreply.github.com>
2023-02-28 22:22:22 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,20 +1,15 @@
 import streamlit as st
 import pymongo
 
-#client = pymongo.MongoClient("mongodb://jiayeelim:@127.0.0.1")
-#db = client["SRA"]
-
-def init_connection():
-    return pymongo.MongoClient(**st.secrets["mongo"])
-
-client = init_connection()
+client = pymongo.MongoClient("mongodb://localhost:27017/")
 db = client.SRA
+
 
 # Accept user input
 user_input = st.text_input("Enter data to be stored in MongoDB:")
 
 if st.button("Submit"):
     # Insert the data into MongoDB
-    prediction = db["test"]
+    prediction = db.test
     prediction.insert_one({"data": user_input})
     st.success("Data has been stored in MongoDB!")
