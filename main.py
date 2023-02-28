8aef3a46ddf7f1865d6c832f5344922e378a3c29
jiayeelim <73089478+jiayeelim@users.noreply.github.com>
2023-02-28 22:10:58 +0800

    Try with sample code

---
+++ main.py
@@ -0,0 +1,14 @@
+import streamlit as st
+import pymongo
+
+client = pymongo.MongoClient("mongodb://localhost:27017/")
+db = client["SRA"]
+
+# Accept user input
+user_input = st.text_input("Enter data to be stored in MongoDB:")
+
+if st.button("Submit"):
+    # Insert the data into MongoDB
+    prediction = db["test"]
+    prediction.insert_one({"data": user_input})
+    st.success("Data has been stored in MongoDB!")
