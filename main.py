a3c71e09db04ff5eb5bfe406b0d223eff3ea1c68
jiayeelim <73089478+jiayeelim@users.noreply.github.com>
2023-02-28 22:23:48 +0800

    Update main.py

--- main.py
+++ main.py
@@ -10,6 +10,6 @@ user_input = st.text_input("Enter data to be stored in MongoDB:")
 
 if st.button("Submit"):
     # Insert the data into MongoDB
-    prediction = db.test
+    prediction = db.mergeAssessment
     prediction.insert_one({"data": user_input})
     st.success("Data has been stored in MongoDB!")
